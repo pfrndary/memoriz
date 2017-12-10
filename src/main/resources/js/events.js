2e39dad3bf676c60a616872bce211e3c2c9f9538
pfrndary <Conquerors8>
2017-12-10 17:43:40 -0500

    buggy async

--- src/main/resources/js/events.js
+++ src/main/resources/js/events.js
@@ -1,64 +1,98 @@
 var cardsdisplayed = [];
+var locks = [];
+var msgQueue = [];
 
 function rotate(event) {
-    rotateObj(event.currentTarget);
+    var id = event.currentTarget.id;
+    if (!locks[id]) {
+        locks[id] = true;
+        msgQueue.push(function() {
+            rotateObj(id);
+            locks[id] = false;
+        });
+    }
+    waitAtLeastOneEvent();
 }
 
-function rotateObj(obj) {
+function rotateObj(id) {
+    var obj = document.getElementById(id);
     if (obj.className.length > 15) {
         if (cardsdisplayed[0].id == obj.id) {
-            hideCard(0);
+            hideCardById(obj.id);
         } else if (cardsdisplayed[1].id == obj.id) {
-            hideCard(1);
+            hideCardById(obj.id);
         }
     } else {
         if (cardsdisplayed[0]) {
-            cardsdisplayed[1] = obj;
-            rotateAndValidateWith(cardsdisplayed[1], cardsdisplayed[0]);
+            msgQueue.push(function() {
+                cardsdisplayed[1] = obj;
+                rotateAndValidateWith(cardsdisplayed[1], cardsdisplayed[0]);
+            });
         } else if (cardsdisplayed[1]) {
-            cardsdisplayed[0] = obj;
-            rotateAndValidateWith(cardsdisplayed[0], cardsdisplayed[1]);
+            msgQueue.push(function() {
+                cardsdisplayed[0] = obj;
+                rotateAndValidateWith(cardsdisplayed[0], cardsdisplayed[1]);
+            });
         } else {
-            cardsdisplayed[0] = obj;
-            showCard(obj);
+            msgQueue.push(function() {
+                cardsdisplayed[0] = obj;
+                showCard(obj);
+            });
         }
+        waitAtLeastOneEvent();
+        waitAtLeastOneEvent();
     }
 }
 
-function hideCard(idx) {
-    cardsdisplayed[idx].className = cardsdisplayed[idx].className.substring(0, cardsdisplayed[idx].className.length -  " rotate".length);
-    if (idx === 0) {
-        cardsdisplayed[0] = null;
-    } else if (idx === 1) {
-        cardsdisplayed[1] = null;
-    }
+function hideCardById(cardId) {
+    console.log(cardId);
+    var card = document.getElementById(cardId);
+    card.className = card.className.substring(0, card.className.length -  " rotate".length);
 }
 
 function showCard(card) {
     card.className += " rotate";
 }
 
-var reactionLose = function(event) {
-        event.currentTarget.removeEventListener("transitionend", reactionLose);
-        var myTimer = setTimeout(function() {
-            hideCard(0);
-            hideCard(1);
-            window.clearTimeout(myTimer);
+var reactionLose = function(id1, id2) {
+    var t = setTimeout(function() {
+            // window.clearTimeout(t);
+            hideCardById(id1);
+            hideCardById(id2);
+            cardsdisplayed[0] = null;
+            cardsdisplayed[1] = null;
         },700);
     };
-var reactionWin = function(event) {
-        event.currentTarget.removeEventListener("transitionend", reactionWin);
-        cardsdisplayed[0].removeEventListener("click", rotate);
-        cardsdisplayed[1].removeEventListener("click", rotate);
-        cardsdisplayed[0] = null;
-        cardsdisplayed[1] = null;
-    };
+
+var reactionWin = function(id1, id2) {
+    console.log('remove event click for '+id1);
+    document.getElementById(id1).removeEventListener("click", rotate);
+    console.log('remove event click for '+id2);
+    document.getElementById(id2).removeEventListener("click", rotate);
+    cardsdisplayed[0] = null;
+    cardsdisplayed[1] = null;
+};
+
 
 function rotateAndValidateWith(card1, card2) {
     if (cardEqual(card1, card2)) {
-        card1.addEventListener("transitionend", reactionWin, false);
+        var fnc = function(e) {
+            msgQueue.push(function() {
+                reactionWin(card1.id, card2.id);
+                card1.removeEventListener("transitionend", fnc);
+            });
+            waitAtLeastOneEvent();
+        };
+        card1.addEventListener("transitionend", fnc, false);
     } else {
-        card1.addEventListener("transitionend", reactionLose, false);
+        var fnc = function(e) {
+            msgQueue.push(function() {
+                reactionLose(card1.id, card2.id);
+                card1.removeEventListener("transitionend", fnc);
+            });
+            waitAtLeastOneEvent();
+        };
+        card1.addEventListener("transitionend", fnc, false);
     }
     showCard(card1);
 }
@@ -71,6 +105,25 @@ var array = document.getElementsByName("card");
 for (var i = 0 ; i < array.length ; i++) {
     var object = array[i];
     object.addEventListener("click", rotate);
+}
+
+var myTimer = null;
+function waitAtLeastOneEvent() {
+    var event = null;
+    myTimer = setTimeout(function() {
+        event = msgQueue.shift();
+        if (event) {
+            // window.clearTimeout(myTimer);
+            myTimer = null;
+            event();
+        }
+        while (event) {
+            event = msgQueue.shift();
+            if (event) {
+                event();
+            }
+        }
+    }, 20);
 }
 
 
--- src/main/resources/js/vue-config.js
+++ src/main/resources/js/vue-config.js
@@ -1,11 +1,32 @@
+
+var imgs = [
+    '../image/a.png',
+    '../image/b.png'
+];
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+}
+var cardsData = [];
+for (var j = 0 ; j < imgs.length ; j++) {
+    cardsData[j] = {  idImg:j, pathImg: imgs[j] };
+}
+for (var j = 0 ; j < imgs.length ; j++) {
+    cardsData[imgs.length+j] = {  idImg:j, pathImg: imgs[j] };
+}
+
+shuffle(cardsData);
+
+
 var app = new Vue({
   el: '#app',
   data: {
-    todos: [
-      { idImg:1, pathImg: '../image/a.png' },
-      { idImg:1, pathImg: '../image/a.png' },
-      { idImg:1, pathImg: '../image/a.png' },
-      { idImg:4, pathImg: '../image/b.png' }
-    ]
+    todos: cardsData
   }
 })
