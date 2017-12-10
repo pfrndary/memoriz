5686a47988652772c8df451cf27c05d908c94fa7
pfrndary <Conquerors8>
2017-12-09 23:40:00 -0500

    new js file

---
+++ src/main/resources/js/vue-config.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    todos: [
+      { pathImg: '../image/a.png' },
+      { pathImg: '../image/a.png' },
+      { pathImg: '../image/a.png' },
+      { pathImg: '../image/b.png' }
+    ]
+  }
+})
